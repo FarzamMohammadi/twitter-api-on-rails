5795025e31782279705595a479520436ca37dcc1
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-01-14 22:35:54 -0500

    users post method complete with password validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,23 @@ module Api
         users = User.order('created_at DESC');
         render json: {status:'SUCCESS', message:'Loaded users', data:users},status: :ok
       end
+
+      def create
+        user = User.new(user_params);
+
+        if user.save
+          render json: {status:'SUCCESS', message:'User added', data:user},status: :ok
+        else
+          render json: {status:'ERROR', message:'User could not be added',
+          data:user.errors},status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.permit(:username, :password)
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
 class User < ApplicationRecord
+  PASSWORD_FORMAT = /\A
+    (?=.{8,})          # Must contain 8 or more characters
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[A-Z])        # Must contain an upper case character
+    (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x
+
+  validates :password,
+  presence: true,
+  length: { in: Devise.password_length },
+  format: { with: PASSWORD_FORMAT },
+  confirmation: true,
+  on: :create
+
+validates :password,
+  allow_nil: true,
+  length: { in: Devise.password_length },
+  format: { with: PASSWORD_FORMAT },
+  confirmation: true,
+  on: :update
+
+  validates :username,
+  presence: true
+
 end
