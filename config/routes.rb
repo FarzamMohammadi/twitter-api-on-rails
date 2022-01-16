68e26369a79a7ab3a8f98424a5257eca319b24fb
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-01-16 18:10:16 -0500

    rspec & factorybot gems added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :users, :chats, :tweets
+      resources :users, only: [:create]
+      resources :chats, only: [:index, :create]
+      resources :tweets, only: [:index, :create, :update, :destroy]
     end
   end
 end
