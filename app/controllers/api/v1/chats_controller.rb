5de8820b7819bafd5d1e9c917f0f89db8db5cab1
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-01-15 20:34:18 -0500

    chat api post method refactored to check for receiver before inserting new record

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -2,12 +2,18 @@ module Api
   module V1
     class ChatsController < ApplicationController
       def create
-        new_chat = Chat.new(sender: session[:user], receiver: chat_params[:receiver] , message: chat_params[:message], read: false);
-        if new_chat.save
-          render json: {status:'SUCCESS', message:'Message sent', data:new_chat},status: :ok
+        # Checks to see if receiver exists in DB - not necessary - thought it would make api more practical
+        exists = User.find_by(username: chat_params[:receiver])
+        if !exists.nil?
+          new_chat = Chat.new(sender: session[:user], receiver: chat_params[:receiver] , message: chat_params[:message], read: false);
+          if new_chat.save
+            render json: {status:'SUCCESS', message:'Message sent', data:new_chat},status: :ok
+          else
+            render json: {status:'ERROR', message:'Message could not be added',
+            data:new_chat.errors},status: :unprocessable_entity
+          end
         else
-          render json: {status:'ERROR', message:'Message could not be added',
-          data:new_chat.errors},status: :unprocessable_entity
+          render json: {status:'ERROR', message:'Receiver does not exist'},status: :bad_request
         end
       end
 
