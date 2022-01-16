bf67f259286296c680cb86406ef171bb54640c5b
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-01-15 22:16:04 -0500

    tweets api complete with crud

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -18,20 +18,35 @@ module Api
         if new_tweet.save
           render json: {status:'SUCCESS', message:'Tweets sent', data:new_tweet},status: :ok
         else
-          render json: {status:'SUCCESS', message:'Tweets could not be sent', data:new_tweet.errors},status: :ok
+          render json: {status:'ERROR', message:'Tweets could not be sent', data:new_tweet.errors},status: :unprocessable_entity
         end
       end
-      def edit
+      def update
 
+        user_tweet = Tweet.find(params[:id])
+        if user_tweet.username == session[:user]
+          user_tweet.description = tweet_params[:description]
+          user_tweet.save
+          render json: {status:'SUCCESS', message:'Tweet updated', data:user_tweet},status: :ok
+        else
+          render json: {status:'ERROR', message:'You do not have permission'},status: :unauthorized
+        end
       end
       def destroy
+        user_tweet = Tweet.find(params[:id])
+        if user_tweet.username == session[:user]
+          user_tweet.delete
+          render json: {status:'SUCCESS', message:'Tweet deleted', data:user_tweet},status: :ok
+        else
+          render json: {status:'ERROR', message:'You do not have permission'},status: :unauthorized
+        end
 
       end
 
       private
 
       def tweet_params
-        params.require(:tweet).permit(:description, :like, :retweet)
+        params.require(:tweet).permit(:description, :like, :retweet, :id)
       end
     end
   end
